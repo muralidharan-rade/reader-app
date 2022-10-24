f77944a88c336bf95d96b811ab13b32f20d85d8d
muralidharan-rade <muralidharan.balanandan@walmart.com>
2022-10-24 12:34:22 +0530

    first commit

---
+++ src/main/java/com/reader/readerapp/ReaderAppApplication.java
@@ -0,0 +1,13 @@
+package com.reader.readerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reader/readerapp/ReaderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reader.readerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
